eb46065eec2b0f7cb20c647f34dfd0fce86353d1
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-08 17:13:32 +0100

    modal style added

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
+  @Input() show: boolean;
+  @Input() title: string;
+  
   constructor() { }
 
   ngOnInit() {
